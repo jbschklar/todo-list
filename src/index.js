43389cbf6ff80223c7c08bc7d0476b5b06270c93
jbschklar <jbschklar@gmail.com>
2022-08-22 16:35:38 -0400

    added some functionality to toggle dropdown lists

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@
 import _ from "lodash";
 import "./style.css";
 
-console.log("working");
+const collapseBtns = document.querySelectorAll(".collapsible");
+
+collapseBtns.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		console.log("click");
+		const list = e.target.closest(".btn-lable").nextElementSibling;
+
+		list.classList.toggle("active");
+		console.log(list.classList);
+	});
+});
+
+console.log(collapseBtns);
